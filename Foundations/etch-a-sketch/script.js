203622531fc2533bb10209e7eec3ca38584ccf20
Allex3 <alex.iordachioaiei3@gmail.com>
2024-08-13 16:03:05 +0300

    Add style to the div grid so it looks 16x16
    Also deal with border and width shenanigans

--- Foundations/etch-a-sketch/script.js
+++ Foundations/etch-a-sketch/script.js
@@ -3,10 +3,29 @@ const container = document.querySelector(".container");
 const divInGrid = document.createElement("div");
 const gridLine = document.createElement("div");
 
+divInGrid.classList.toggle("squareDivInGrid");
+gridLine.classList.toggle("gridLine");
+//added the css classes for styling 
+
 for (let i=0; i<16; i++) //16 squares on the grid line
-    gridLine.appendChild(divInGrid);
+{
+    const divClone = divInGrid.cloneNode(true);
+    if (i==0) divClone.classList.toggle("leftBorder");
+    if (i==15) divClone.classList.toggle("rightBorder");
+    gridLine.appendChild(divClone);
+}
 
 //now add the 16 lines to the main container div 
-for (let i=0; i<16; i++)
-    container.appendChild(gridLine); 
+for (let i=0; i<16; i++) {
+    const lineClone = gridLine.cloneNode(true);
+    if (i==0) lineClone.classList.toggle("topBorder");
+    if (i==15) lineClone.classList.toggle("bottomBorder"); 
+    container.appendChild(lineClone); 
+}
 
+//leftBorder, etc. are classes for the remained border on top, left, bottom, right
+//because the borders intertwined at the other div squares, on those at the edges they don't
+//so the borders remain thinner
+
+//first time using cloneNode, didn't know I couldn't append it directly multiple times
+//but it's ONE node created so it makes sense... it's a reference
