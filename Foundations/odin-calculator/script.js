2fcb84494e13811fe7d0d3487de58bd1818e7a40
Allex3 <alex.iordachioaiei3@gmail.com>
2024-08-14 16:20:50 +0300

    Add calculator buttons and flex them in a list

---
+++ Foundations/odin-calculator/script.js
@@ -0,0 +1,16 @@
+function add(a, b, op) {
+    const 
+}
+function subtract(a, b, op) {
+    
+}
+function multiply(a, b, op) {
+    
+}
+function divide(a, b, op) {
+    
+}
+
+function operate(op, a, b) {
+    
+}
